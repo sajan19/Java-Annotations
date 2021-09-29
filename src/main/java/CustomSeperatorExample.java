b8678d645160b2e12c01a7c51269ac6e03e4d473
Sajan <sajanmhatre29@gmail.com>
2021-09-29 12:13:18 +0530

    Custom Separator

--- src/main/java/CustomSeperatorExample.java
+++ src/main/java/CustomSeperatorExample.java
@@ -14,7 +14,6 @@ public class CustomSeperatorExample{
         CSVReader reader = new CSVReader(new FileReader(filePath));
         //Read all rows at once
         List<String[]> allRows = reader.readAll();
-
         //Read CSV line by line and use the string array as you want
         for(String[] row : allRows){
             System.out.println(Arrays.toString(row));
